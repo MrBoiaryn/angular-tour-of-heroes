236963e50878c6da7489c26501be7f62b5b6a7ad
Mr.Boiaryn <mr.boiaryn@gmail.com>
2024-11-27 13:46:36 +0200

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { HeroesComponent } from './components/heroes/heroes.component';
 import { MessagesComponent } from './components/messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, MessagesComponent, RouterLink],
+  imports: [RouterOutlet, MessagesComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
